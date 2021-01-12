941d65b9306eb0b9705df1c8041eef3ac5a0de9b
Kay Warrie <kaywarrie@gmail.com>
2021-01-12 13:28:34 +0100

    update docs

--- portal2dcat/config.py
+++ portal2dcat/config.py
@@ -2,11 +2,11 @@ import os
 
 #globals
 
-## URLSs
+## URLs
 AGS_PORT = "https://portaal-stadantwerpen.opendata.arcgis.com"
 AOD_LIC = "https://www.antwerpen.be/nl/overzicht/open-data/over-opendata/gratis-open-data-licentie"
 CONTACT = "https://www.antwerpen.be/nl/overzicht/open-data/over-opendata/contacteer-open-data"
 HOME_URL = "https://antwerpen.be"
 
-## defauls
-OUT_PATH = os.path.dirname(__file__) + ".xml"
+## defaults
+OUT_PATH = "dcat.xml"
